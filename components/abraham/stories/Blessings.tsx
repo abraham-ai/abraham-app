8b35b0461d35096fb7898152f3776644659bb6ab
genekogan <kogan.gene@gmail.com>
2024-10-17 21:42:52 -0400

    blessings

--- components/abraham/stories/Blessings.tsx
+++ components/abraham/stories/Blessings.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 
-export default function Blessings({ blessings }: { blessings: string[] }) {
+interface Blessing {
+  user: string;
+  blessing: string;
+}
+
+export default function Blessings({ blessings }: { blessings: Blessing[] }) {
   return (
-    <div className="flex flex-col items-center justify-center ">
+    <div className="flex flex-col items-center justify-center">
       {blessings.map((blessing, index) => (
         <div
           key={index}
           className="flex flex-col border-b border-gray-300 p-6 lg:w-[43vw]"
         >
-          <div className="text-gray-700">{blessing}</div>
+          <div className="text-gray-700">
+            <span className="font-bold">{blessing.user}: </span>
+            {blessing.blessing}
+          </div>
         </div>
       ))}
     </div>
