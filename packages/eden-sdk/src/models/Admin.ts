2c5457ab82289d315550c075da06c490dce41a76
bashybaranaba <bashybaranaba@gmail.com>
2025-08-24 21:18:06 +0300

    fix:work with correct sdk

--- packages/eden-sdk/src/models/Admin.ts
+++ packages/eden-sdk/src/models/Admin.ts
@@ -3,168 +3,168 @@ import {
   WebAPICallOptions,
   WebAPICallResult,
   transformArgsIntoURLParams,
-} from "../types";
-import { AxiosRequestConfig } from "axios";
-import { ApiKey } from "src/models/ApiKeys";
+} from '../types'
+import { AxiosRequestConfig } from 'axios'
+import { ApiKey } from 'src/models/ApiKeys'
 
 export interface AdminMannaModifyArguments extends WebAPICallOptions {
-  userId: string;
-  amount: number;
+  userId: string
+  amount: number
 }
 
 export interface AdminMannaVouchersCreateArguments extends WebAPICallOptions {
-  amount: number;
-  allowedUserIds?: string[];
-  numberOfUses?: number;
-  code?: string;
+  amount: number
+  allowedUserIds?: string[]
+  numberOfUses?: number
+  code?: string
 }
 
 export interface AdminAuthChallengeArguments extends WebAPICallOptions {
-  address: string;
-  isWeb2: boolean;
+  address: string
+  isWeb2: boolean
 }
 
 export interface AdminMediaUploadArguments extends WebAPICallOptions {
-  media: Buffer;
+  media: Buffer
 }
 
 export interface AdminConceptsExportToHFArguments extends WebAPICallOptions {
-  conceptId: string;
-  userId: string;
+  conceptId: string
+  userId: string
 }
 
 export interface AdminConceptsRemoveFromHFArguments extends WebAPICallOptions {
-  conceptId: string;
-  userId: string;
+  conceptId: string
+  userId: string
 }
 
 export interface AdminApiKeysListArguments extends WebAPICallOptions {
-  character: string | string[];
+  character: string | string[]
 }
 
 // Requests
 
 export const adminMannaModifyRequestConfig = (
-  args: AdminMannaModifyArguments
+  args: AdminMannaModifyArguments,
 ): AxiosRequestConfig => {
   return {
-    method: "POST",
-    url: "admin/manna/modify",
+    method: 'POST',
+    url: 'admin/manna/modify',
     data: {
       ...args,
     },
-  };
-};
+  }
+}
 
 export const adminMannaVouchersCreateRequestConfig = (
-  args: AdminMannaVouchersCreateArguments
+  args: AdminMannaVouchersCreateArguments,
 ): AxiosRequestConfig => {
   return {
-    method: "POST",
-    url: "admin/manna/vouchers/create",
+    method: 'POST',
+    url: 'admin/manna/vouchers/create',
     data: {
       ...args,
     },
-  };
-};
+  }
+}
 
 export const adminAuthChallengeRequestConfig = (
-  args: AdminAuthChallengeArguments
+  args: AdminAuthChallengeArguments,
 ): AxiosRequestConfig => {
   return {
-    method: "POST",
-    url: "admin/auth/challenge",
+    method: 'POST',
+    url: 'admin/auth/challenge',
     data: {
       ...args,
     },
-  };
-};
+  }
+}
 
 export const adminMediaUploadRequestConfig = (
-  args: AdminMediaUploadArguments
+  args: AdminMediaUploadArguments,
 ) => {
-  const blob = new Blob([args.media] as any);
-  const form = new FormData();
-  form.append("file", blob);
+  const blob = new Blob([args.media])
+  const form = new FormData()
+  form.append('file', blob)
   return {
-    method: "POST",
-    url: "media/upload",
+    method: 'POST',
+    url: 'media/upload',
     data: form,
     headers: {
-      "Content-Type": "multipart/form-data",
+      'Content-Type': 'multipart/form-data',
     },
-  };
-};
+  }
+}
 
 export const adminConceptsExportToHFRequestConfig = (
-  args: AdminConceptsExportToHFArguments
+  args: AdminConceptsExportToHFArguments,
 ): AxiosRequestConfig => {
   return {
-    method: "POST",
+    method: 'POST',
     url: `admin/concepts/export/hf`,
     data: {
       ...args,
     },
-  };
-};
+  }
+}
 
 export const adminConceptsRemoveFromHFRequestConfig = (
-  args: AdminConceptsRemoveFromHFArguments
+  args: AdminConceptsRemoveFromHFArguments,
 ): AxiosRequestConfig => {
   return {
-    method: "POST",
+    method: 'POST',
     url: `admin/concepts/remove/hf`,
     data: {
       ...args,
     },
-  };
-};
+  }
+}
 
 export const adminApiKeysListRequestConfig = (
-  args: AdminApiKeysListArguments
+  args: AdminApiKeysListArguments,
 ): AxiosRequestConfig => {
-  const params = transformArgsIntoURLParams(args);
+  const params = transformArgsIntoURLParams(args)
   return {
-    method: "GET",
-    url: "admin/apikeys",
+    method: 'GET',
+    url: 'admin/apikeys',
     params,
-  };
-};
+  }
+}
 
 // Responses
 
 export type AdminMannaModifyResponse = WebAPICallResult & {
-  error?: string;
-  userId?: string;
-  manna?: number;
-  transactionId?: string;
-};
+  error?: string
+  userId?: string
+  manna?: number
+  transactionId?: string
+}
 
 export type AdminMannaVouchersCreateResponse = WebAPICallResult & {
-  error?: string;
-  code?: string;
-};
+  error?: string
+  code?: string
+}
 
 export type AdminAuthChallengeResponse = WebAPICallResult & {
-  error?: string;
-  nonce?: string;
-};
+  error?: string
+  nonce?: string
+}
 
 export type AdminMediaUploadResponse = WebAPICallResult & {
-  error?: string;
-  url?: string;
-};
+  error?: string
+  url?: string
+}
 
 export type AdminConceptsExportToHFResponse = WebAPICallResult & {
-  error?: string;
-  url?: string;
-};
+  error?: string
+  url?: string
+}
 
 export type AdminConceptsRemoveFromHFResponse = WebAPICallResult & {
-  error?: string;
-};
+  error?: string
+}
 
 export type AdminApiKeysListResponse = WebAPICallResult &
   PaginatedResponse<ApiKey> & {
-    error?: string;
-  };
+    error?: string
+  }
